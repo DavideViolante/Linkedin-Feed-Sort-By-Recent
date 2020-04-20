07de7dab27b04f339200124888d2545c601339e3
DavideViolante <fungtq3@gmail.com>
2020-04-20 23:11:24 +0200

    Update to make it work always

--- Chrome Extension/LinkedinFeedSortByRecent/linkedinFeedSortByRecent.js
+++ Chrome Extension/LinkedinFeedSortByRecent/linkedinFeedSortByRecent.js
@@ -3,12 +3,13 @@
 // Author:          Davide Violante - https://github.com/DavideViolante/		
 // DownloadURL:     https://github.com/DavideViolante/Linkedin-Feed-Sort-By-Recent/		
 
-
 (function () {
   document.cookie = 'UpdateFinder=chronFeed; domain=www.linkedin.com; path=/';
   const topOrRecentElem = document.querySelector('.mh1');
   if (topOrRecentElem && topOrRecentElem.textContent.trim() === 'Top') {
-    const recentDropdownItem = document.getElementById('ember61');
-    recentDropdownItem.click();
+    // Open the dropdown menu
+    topOrRecentElem.click();
+    // Click Recent from the dropdown menu
+    document.querySelectorAll('.mv1')[1].click();
   }
 })();
--- Userscript/linkedinFeedSortByRecent.user.js
+++ Userscript/linkedinFeedSortByRecent.user.js
@@ -2,7 +2,7 @@
 // @name            Linkedin Feed sort by Recent
 // @description     Automatically set Sort by Recent on Linkedin feed.
 // @icon            https://static-exp1.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca
-// @version         2020.04.11 (1.0)
+// @version         2020.04.20 (1.1)
 // @namespace       linkedinfeedsortbyrecent
 // @author          https://github.com/DavideViolante/
 // @downloadURL     https://github.com/DavideViolante/Linkedin-Feed-Sort-By-Recent/raw/master/Userscript/linkedinFeedSortByRecent.user.js
@@ -14,7 +14,9 @@
   document.cookie = 'UpdateFinder=chronFeed; domain=www.linkedin.com; path=/';
   const topOrRecentElem = document.querySelector('.mh1');
   if (topOrRecentElem && topOrRecentElem.textContent.trim() === 'Top') {
-    const recentDropdownItem = document.getElementById('ember61');
-    recentDropdownItem.click();
+    // Open the dropdown menu
+    topOrRecentElem.click();
+    // Click Recent from the dropdown menu
+    document.querySelectorAll('.mv1')[1].click();
   }
 })();
